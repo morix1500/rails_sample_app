63d85ce122fbaf1e55794515086106b9963a4851
morix1500 <rade1500@gmail.com>
2018-03-02 00:10:36 +0000

    add password reset test

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,7 @@ class User < ApplicationRecord
   end
   
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
   
   def remember
@@ -36,8 +35,7 @@ class User < ApplicationRecord
   
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
   
   def authenticated?(attribute, token)
